3b8cde98f5ae8cd61e3e1fe3a7114ccd4e620a13
rena <renalynch@renalynch.com>
2020-05-25 19:12:03 -0700

    Updates

---
+++ precache-manifest.dce851aff83f40239fd26b86b999eb93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c4b5822ab70bb3c1abb92f33c1d10c",
+    "url": "/Juans-El-Caporal/index.html"
+  },
+  {
+    "revision": "f1f8126993a5f4a6c6a1",
+    "url": "/Juans-El-Caporal/static/css/2.38eed453.chunk.css"
+  },
+  {
+    "revision": "21fc1af4edfcc30abdc3",
+    "url": "/Juans-El-Caporal/static/css/main.10a61be1.chunk.css"
+  },
+  {
+    "revision": "f1f8126993a5f4a6c6a1",
+    "url": "/Juans-El-Caporal/static/js/2.d4d97300.chunk.js"
+  },
+  {
+    "revision": "21fc1af4edfcc30abdc3",
+    "url": "/Juans-El-Caporal/static/js/main.e539142d.chunk.js"
+  },
+  {
+    "revision": "a74e4767b97486d34b62",
+    "url": "/Juans-El-Caporal/static/js/runtime-main.df9ffbcb.js"
+  },
+  {
+    "revision": "06b1626ec194ae3be951d8f3ca79cc10",
+    "url": "/Juans-El-Caporal/static/media/JUANS.06b1626e.png"
+  },
+  {
+    "revision": "ae321421f7c8efa6407cbcfb53a92c3b",
+    "url": "/Juans-El-Caporal/static/media/juansaddr.ae321421.png"
+  },
+  {
+    "revision": "882450e2173febfacc83e60e2a41d990",
+    "url": "/Juans-El-Caporal/static/media/juanspho.882450e2.png"
+  }
+]);
